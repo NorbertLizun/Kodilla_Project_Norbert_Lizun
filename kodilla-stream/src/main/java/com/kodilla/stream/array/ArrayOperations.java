aff3f3fdf4ee87ea56f165ddec37ba79a75ee87b
unknown <norbert.a.lizun.official@gmail.com>
2020-11-23 22:50:41 +0100

    7.5 Obliczamy srednia funkcyjnie

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,4 +1,20 @@
 package com.kodilla.stream.array;
 
-public interface ArrayOperations {
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+public  interface ArrayOperations {
+    static double getAverage(int[] numbers) {
+
+        IntStream.range(0, numbers.length)
+            .map(n -> numbers[n])
+            .forEach(System.out::println);
+
+        OptionalDouble average = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average();
+
+        double value = average.orElse(-1);
+        return value;
+    }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,5 +1,22 @@
 package com.kodilla.stream.array;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static com.kodilla.stream.array.ArrayOperations.getAverage;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArrayOperationsTestSuite {
-    
+
+    @Test
+    void testGetAverage() {
+        //Given
+        int[] numbers = {1,3,4,5,6,2,2,3,1,4,5,3,2,1,3,4,2,1,3,4};
+
+        //When
+        double average = getAverage(numbers);
+        //Then
+        assertEquals(2.95, average);
+    }
 }
