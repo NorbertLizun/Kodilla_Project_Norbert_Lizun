12249779025e8b0c2a3f493d1f4dd5f602db2f70
unknown <norbert.a.lizun.official@gmail.com>
2021-01-20 17:41:24 +0100

    iteracja po tytulach

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,11 @@
+package com.kodilla.good.patterns.challenges;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+
+        movieStore.getAllMovie();
+    }
+}
