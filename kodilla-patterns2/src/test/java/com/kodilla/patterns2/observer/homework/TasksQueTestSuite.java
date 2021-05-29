32664ea2b01a10feb2fe55be37eeb4ff27bbbddb
norbert <norbert.a.lizun.official@gmail.com>
2021-05-29 18:34:57 +0200

    Observer task fixed

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/TasksQueTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/TasksQueTestSuite.java
@@ -44,26 +44,3 @@ public class TasksQueTestSuite {
     }
 }
 
-//    //Given
-//    Student student1 = new Student("Anna", "Nowak");
-//    Student student2 = new Student("Jan", "Kowalski");
-//
-//    HomeworkTask task1 = new HomeworkTask("Task1", "Description1");
-//    HomeworkTask task2 = new HomeworkTask("Task2", "Description2");
-//    HomeworkTask task3 = new HomeworkTask("Task3", "Description3");
-//
-//    HomeworkTasksQueue queue1 = student1.getHomeworkTasksQueue();
-//    HomeworkTasksQueue queue2 = student2.getHomeworkTasksQueue();
-//
-//    Mentor mentor = new Mentor("Wojciech", "Kodilla");
-//
-//        queue1.registerObserver(mentor);
-//                queue2.registerObserver(mentor);
-//
-//                //When
-//                queue1.addHomeworkTask(task1);
-//                queue2.addHomeworkTask(task2);
-//                queue2.addHomeworkTask(task3);
-//
-//                //Then
-//                assertEquals(3, mentor.getQuantityOfActualTasks());
